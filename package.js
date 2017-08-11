dbb0972a7998f2914ae7f83982a3b02d786264eb
Marc Nitzsche <marc.nitzsche@live.de>
2017-08-11 15:37:19 +0200

    Clean up package

--- package.js
+++ package.js
@@ -1,25 +1,18 @@
 Package.describe({
   summary: 'Integrate with Raven JS for JavaScript errors and logs',
-  version: '0.3.3',
-  name: 'dvelopment:raven',
-  git: 'https://github.com/dVelopment/meteor-raven.git',
+  version: '0.4.0',
+  name: 'flowkey:raven',
+  git: 'https://github.com/flowkey/meteor-raven.git',
   description: 'README.md'
 });
 
 Npm.depends({
-  'raven': '1.1.1',
-  'raven-js': '3.10.0',
+  'raven': '2.1.1',
+  'raven-js': '3.17.0',
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.4.2.3');
   api.use('ecmascript');
-  api.mainModule('lib/main.js', [ 'client', 'server' ]);
-
-  api.export([
-    'RavenLogger'
-  ], [
-    'client',
-    'server'
-  ]);
+  api.mainModule('lib/main.js');
 });
